d816d9a6a7a91abe5896acde31213e2ad28eee65
mentalfabric <koomjianschool@gmail.com>
2016-06-29 20:45:38 -0700

    day 2 commit

---
+++ Day 02/app.js
@@ -0,0 +1,9 @@
+/**
+ * Created by robert on 6/29/16.
+ */
+var name = prompt("What is your first name?");
+var lastName = prompt("What is your last name?")
+var question = confirm("Do you like chicken?");
+console.log(name);
+alert(lastName);
+alert(question);
