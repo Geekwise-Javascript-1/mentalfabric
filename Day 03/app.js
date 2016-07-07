8c47ba6b9d140e3ec2d20096b231200ef0266dc0
mentalfabric <koomjianschool@gmail.com>
2016-07-06 20:53:01 -0700

    day 3

---
+++ Day 03/app.js
@@ -0,0 +1,44 @@
+/**
+ * Created by robert on 6/29/16.
+ */
+//var name = prompt("What is your first name?");
+//var lastName = prompt("What is your last name?")
+//var question = confirm("Do you like chicken?");
+//console.log(name);
+//alert(lastName);
+//alert(question);
+
+//var firstNum = prompt('first number');
+//var secondNum = prompt('second number');
+//
+//console.log(firstNum);
+//console.log(secondNum);
+//
+//var thirdNum = parseInt(firstNum);
+//var fourthNum = parseInt(secondNum);
+//
+//var fifthNum = thirdNum + fourthNum;
+//
+//alert(fifthNum);
+
+var a = parseInt(prompt('how old are you?'));
+var b = prompt('what is your gender?');
+//
+//if(a < 18 && b == 'male'){
+//    alert('hello young man');
+//} else if(a < 18 && b == 'female'){
+//    alert('hello young lady');
+//} else if(a > 18 && a < 50 && b == 'male'){
+//    alert('hello sir');
+//} else if (a > 18 && a < 50 && b == 'female'){
+//    alert('hello miss');
+//} else if( a > 50 && b == 'male'){
+//    alert('hey gramps!');
+//} else if(a > 50 && b == 'female'){
+//    alert('hey granny!');
+//}
+
+//switch( a | b){
+//    case (a < 18) | "male":
+//        console.log('test');
+//}
